2f28cf66a0cf06060083c3a216f1f09ca9c01b4b
MinhHuy693 <mHuy693@gmail.com>
2024-08-27 09:33:01 +0700

    Mi_Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Center(
-            child: Text('I Am Rich'),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/user.jpg'),
+              ),
+              const Text(
+                'Angle Tu',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 10.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    Text(
+                      '+44 123 456 789',
+                      style: TextStyle(
+                          color: Colors.teal.shade900, fontSize: 10.0),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    Text(
+                      'angle@email.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900, fontSize: 10.0),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-              image: AssetImage('images/diamond.jpg')),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
