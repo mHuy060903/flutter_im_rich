df6c73d29f5c8492984a22546de77a3b862a782f
MinhHuy693 <mHuy693@gmail.com>
2024-10-01 14:34:06 +0700

    Lab 4

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: const Text('Ask Me Anything', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.black,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
@@ -25,8 +25,7 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePage extends State<DicePage> {
-  var leftDiceNumber = 5;
-  var rightDiceNumber = 1;
+  var numImage = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -38,20 +37,12 @@ class _DicePage extends State<DicePage> {
               child: TextButton(
             onPressed: () {
               setState(() {
-                leftDiceNumber = Random().nextInt(6) + 1;
+                numImage = Random().nextInt(5) + 1;
               });
             },
-            child: Image.asset('images/dice$leftDiceNumber.png'),
+            child: Image.asset('images/ball$numImage.png'),
           )),
-          Expanded(
-              child: TextButton(
-            onPressed: () {
-              setState(() {
-                rightDiceNumber = Random().nextInt(6) + 1;
-              });
-            },
-            child: Image.asset('images/dice$rightDiceNumber.png'),
-          )),
+         
         ],
       ),
     );
