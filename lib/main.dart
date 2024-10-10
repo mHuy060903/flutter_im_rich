e0735b350a1a4728044b2157b00744faaceb0fb4
MinhHuy693 <mHuy693@gmail.com>
2024-10-10 17:58:01 +0700

    Lab 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,72 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title: const Text('Ask Me Anything', style: TextStyle(color: Colors.white),),
-          backgroundColor: Colors.black,
-        ),
-        body: const DicePage(),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.green),
+                ),
+                child: const Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                ),
+                child: const Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.yellow),
+                ),
+                child: const Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.blue),
+                ),
+                child: const Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.purple),
+                ),
+                child: const Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  backgroundColor:
+                      WidgetStatePropertyAll<Color>(Colors.greenAccent),
+                ),
+                child: const Text(''),
+              ),
+            )
+          ],
+        )),
       ),
     ),
   );
-}
-
-class DicePage extends StatefulWidget {
-  const DicePage({super.key});
-  @override
-  State<StatefulWidget> createState() {
-    return _DicePage();
-  }
-}
-
-class _DicePage extends State<DicePage> {
-  var numImage = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-              child: TextButton(
-            onPressed: () {
-              setState(() {
-                numImage = Random().nextInt(5) + 1;
-              });
-            },
-            child: Image.asset('images/ball$numImage.png'),
-          )),
-         
-        ],
-      ),
-    );
-  }
 }
